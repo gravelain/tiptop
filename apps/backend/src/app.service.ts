766567f0f32490d3ca878435cfc2d2c010890146
thierry <thierry.temgoua98@gmail>
2025-03-13 15:46:04 +0100

    test 5 ci

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'hello NestJs is good!';
+    return 'Nest Js API is running ....!';
   }  
 }

80709928543a38c5f47bfdedf23add31f6dd7386
thierry <thierry.temgoua98@gmail>
2025-03-13 08:51:23 +0100

    juste jest config

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -1,17 +1,12 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/tests/jest.setup.js'],
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',  // Utilisé pour les tests frontend (environnements navigateur)
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest', // Transformation des fichiers TS/TSX
-    '^.+\\.(js|jsx)$': 'babel-jest', // Transformation des fichiers JS/JSX
-  },
+  testEnvironment: 'node', 
   testMatch: [
-    '**/src/**/*.test.ts',  // Recherche des fichiers de test avec .test.ts
-    '**/src/**/*.spec.ts',  // Recherche des fichiers de test avec .spec.ts
-    '**/src/**/*.test.tsx', // Recherche des fichiers de test avec .test.tsx
-    '**/src/**/*.spec.tsx', // Recherche des fichiers de test avec .spec.tsx
+    '**/src/**/*.test.ts',
+    '**/src/**/*.spec.ts',
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  transformIgnorePatterns: ['node_modules/(?!(next|react)/)'],  // Ne pas ignorer les fichiers Next.js
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+  },
 };

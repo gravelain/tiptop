9f9010c274ee8c9a18a80d7593cfa783f594c29d
thierry <thierry.temgoua98@gmail>
2025-03-14 13:45:32 +0100

    ajout coverage

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -1,4 +1,3 @@
-// vitest.config.ts
 import { defineConfig } from 'vitest/config';
 import path from 'path';
 
@@ -8,6 +7,12 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/tests/setup.ts'],
     include: ['**/*.{test,spec}.{ts,tsx}'],
+    coverage: {
+      provider: 'v8', // Utilisation de V8 Coverage de google comme fournisseur de coverage
+      reporter: ['text', 'html'], // Afficher les résultats dans le terminal et générer un rapport HTML
+      include: ['src/**/*.{ts,js,tsx,jsx}'], // Inclure tous les fichiers source
+      exclude: ['**/node_modules/**', '**/dist/**'], // Exclure les fichiers non pertinents comme node_modules et dist
+    },
   },
   resolve: {
     alias: {

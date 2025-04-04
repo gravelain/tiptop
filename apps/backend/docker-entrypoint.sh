c2110eb1cbd12b8c79b65755096ecf6e21cfcb9d
thierry <gravelains@gmail.com>
2025-04-04 17:50:47 +0200

    test: sh_entrypoint

--- apps/backend/docker-entrypoint.sh
+++ apps/backend/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Attendre que PostgreSQL soit prêt
-/usr/local/bin/wait-for-postgres.sh postgres 5432 bdd_user bdd_pass
+# Attendre que PostgreSQL soit prêt sur le port 5433
+/usr/local/bin/wait-for-postgres.sh postgres 5433 bdd_user bdd_pass
 
 # Lancer le serveur PHP-FPM
 exec php-fpm

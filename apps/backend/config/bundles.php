3bc1864316a467d61efd903f3789dd327647dd9d
thierry <gravelains@gmail.com>
2025-04-04 05:16:05 +0200

    test: configure_api_plateforme

--- apps/backend/config/bundles.php
+++ apps/backend/config/bundles.php
@@ -10,5 +10,4 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
-    
 ];
